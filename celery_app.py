4594a6a0f0d2e2461003bc80911aca449fbc2dad
MaksymKhatskevych <KhatskevychMaksym@gmail.com>
2025-08-21 01:11:30 +0300

    edit celery_app

--- celery_app.py
+++ celery_app.py
@@ -2,13 +2,6 @@ from celery import Celery
 
 celery_app = Celery(
     "tasks",
-    broker="redis://host.docker.internal:6379/0",  # брокер задач
-    backend="redis://host.docker.internal:6379/1", # результат и статусы задач
-    include=["tasks"]
-)
-
-celery_app = Celery(
-    "tasks",
     broker="redis://default:buLKeHNoBFZARkjVpNAEFbjdRLhiguts@redis.railway.internal:6379/0",  # брокер задач
     backend="redis://default:buLKeHNoBFZARkjVpNAEFbjdRLhiguts@redis.railway.internal:6379/1", # результат и статусы задач
     include=["tasks"]
